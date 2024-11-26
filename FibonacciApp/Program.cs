3bc28a72e0816554c2cc5e8b95c4e33f48a800c8
ivanturik <vnturik@gmail.com>
2024-11-26 11:28:53 +0300

    Добавлен проект для генерации чисел Фибоначчи

---
+++ FibonacciApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using FibonacciLibrary;
+
+namespace FibonacciApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите число n:");
+                int n = int.Parse(Console.ReadLine());
+
+                var generator = new FibonacciGenerator();
+                var context = new FibonacciContext(generator);
+
+                List<long> fibonacci = context.GenerateFibonacci(n);
+
+                Console.WriteLine("Числа Фибоначчи:");
+                Console.WriteLine(string.Join(" ", fibonacci));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: Введите корректное число.");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ FibonacciTests/FibonacciContextTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using FibonacciLibrary;
+using Xunit;
+
+namespace FibonacciTests
+{
+    public class FibonacciContextTests
+    {
+        [Fact]
+        public void GenerateFibonacci_ValidInput_UsesProvidedGenerator()
+        {
+            var generator = new FibonacciGenerator();
+            var context = new FibonacciContext(generator);
+            List<long> result = context.GenerateFibonacci(5);
+            Assert.Equal(new long[] { 0, 1, 1, 2, 3 }, result.ToArray());
+        }
+    }
+}
